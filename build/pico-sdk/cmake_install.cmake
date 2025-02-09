60f6e19cc56721dc3837f1713fd36b558cdc3a3c
Silas Belém Junior <99844904+SilasBelem@users.noreply.github.com>
2025-02-09 09:33:36 -0300

    Build updated

--- build/pico-sdk/cmake_install.cmake
+++ build/pico-sdk/cmake_install.cmake
@@ -39,16 +39,16 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/User/Downloads/Display-com-matriz-leds/Display-com-matriz-leds/build/pico-sdk/tools/cmake_install.cmake")
+  include("C:/Users/User/Downloads/DisplayOLED-Matriz/build/pico-sdk/tools/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/User/Downloads/Display-com-matriz-leds/Display-com-matriz-leds/build/pico-sdk/src/cmake_install.cmake")
+  include("C:/Users/User/Downloads/DisplayOLED-Matriz/build/pico-sdk/src/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/User/Downloads/Display-com-matriz-leds/Display-com-matriz-leds/build/pico-sdk/docs/cmake_install.cmake")
+  include("C:/Users/User/Downloads/DisplayOLED-Matriz/build/pico-sdk/docs/cmake_install.cmake")
 endif()
 
--- build/pico-sdk/src/rp2_common/pico_lwip/cmake_install.cmake
+++ build/pico-sdk/src/rp2_common/pico_lwip/cmake_install.cmake
@@ -39,6 +39,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/User/Downloads/Display-com-matriz-leds/Display-com-matriz-leds/build/pico-sdk/src/rp2_common/pico_lwip/tools/cmake_install.cmake")
+  include("C:/Users/User/Downloads/DisplayOLED-Matriz/build/pico-sdk/src/rp2_common/pico_lwip/tools/cmake_install.cmake")
 endif()
 
